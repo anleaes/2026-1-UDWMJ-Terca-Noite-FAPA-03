9617b3ceea3ef63f6115e2d749b403a5262bb26c
vinicius borba <viniboal28@gmail.com>
2026-06-02 15:24:09 -0300

    fix adjust system login so just one super user can manipulate it

--- apps/accounts/decorators.py
+++ apps/accounts/decorators.py
@@ -2,12 +2,10 @@ from functools import wraps
 from django.shortcuts import redirect
 
 
-def employee_required(view_func):
+def login_required(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect('accounts:login')
-        if not hasattr(request.user, 'employee_profile'):
             return redirect('accounts:login')
         return view_func(request, *args, **kwargs)
     return wrapper
