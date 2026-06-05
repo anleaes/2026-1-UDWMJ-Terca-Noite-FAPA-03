b886241ce3970a9560f97d686f4699fa7d8978f3
Eduardo <almeidabedu@gmail.com>
2026-06-05 16:36:17 -0300

    add vue urls

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,4 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('api-login/', views.api_login, name='api_login'),
+    path('api-me/', views.api_me, name='api_me'),
+    path('api-logout/', views.api_logout, name='api_logout'),
 ]
